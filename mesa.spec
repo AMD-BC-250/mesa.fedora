AMD-BC-250/mesa.fedora

commit 7911753afd8fd96866fd5a933b7720084f31d118

Disable asahi in RHEL builds

mesa.spec
@@ -44,8 +44,8 @@
 %global with_vc4       1
 %global with_etnaviv   1
 %global with_tegra     1
-%endif
 %global with_asahi     1
+%endif
 %global with_freedreno 1
 %global with_panfrost  1
 %global with_v3d       1

commit 02448c7c1fd7fa6850ae3b490bc6c0db253ae9b2

Fix failure for Asahi disabled in RHEL builds
The Apple DRI driver is part of the KMSRO framework and is always
built when any KMSRO driver is enabled. This is expressed as a
symlink file named "apple_dri.so" pointing to the main driver file.

When we aren't shipping the asahi driver, we do not need this file,
so delete it in that case.

Fixes: 7911753afd8fd96866fd5a933b7720084f31d118 ("Disable asahi in RHEL builds")

mesa.spec
@@ -446,6 +446,12 @@ rm -vf %{buildroot}%{_libdir}/libEGL_mesa.so
 # XXX can we just not build this
 rm -vf %{buildroot}%{_libdir}/libGLES*
 
+%if ! 0%{?with_asahi}
+# This symlink is unconditionally created when any kmsro driver is enabled
+# We don't want this one so delete it
+rm -vf %{buildroot}%{_libdir}/dri/apple_dri.so
+%endif
+
 # glvnd needs a default provider for indirect rendering where it cannot
 # determine the vendor
 ln -s %{_libdir}/libGLX_mesa.so.0 %{buildroot}%{_libdir}/libGLX_system.so.0